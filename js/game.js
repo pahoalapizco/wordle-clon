39bcac153597d23965e87151f66570568f6d1026
Paola Alapizco <paolac.alapizco@gmail.com>
2022-06-01 17:00:24 -0600

    Footer added, delay in win alert

--- js/game.js
+++ js/game.js
@@ -1,51 +1,53 @@
 // const letterBoard = document.getElementsByClassName("letter--container");
 
 const MAX_TRIES = 6,
-      MAX_LETTERS = 5;
-const secretword = "PAOLA".toUpperCase().split('');
+  MAX_LETTERS = 5;
+const secretword = "ARBOL".toUpperCase().split("");
 const responses = [];
 
 // variables de control
 let currentResponse = [],
-    totalTries = 0,
-    totalLetters = 0,
-    win = false;
+  totalTries = 0,
+  totalLetters = 0,
+  win = false;
 
-const chekLetter = () => {
+const checkAnswer = () => {
+  const letterBoard = document.getElementsByClassName(
+    `letter-row-${totalTries}`
+  );
+  let totalCorrect = 0;
 
-}
-
-const send = () => {
-  if(MAX_TRIES === totalTries) {
-    alert(`Game is over :( you ${win ? "WIN 🥳 " : "Loose  🥺"}`);
-    return; 
+  for (let index = 0; index < MAX_LETTERS; index++) {
+    const letter = secretword[index];
+      if(letter === currentResponse[index]) {
+        letterBoard[index].classList.add("correct");
+        totalCorrect++;
+      } else if(currentResponse.includes(letter)) {
+        letterBoard[index].classList.add("wrong-position");
+      } else {
+        letterBoard[index].classList.add("incorrect");
+      }
   }
 
-  if(MAX_LETTERS > totalLetters) {
+  return totalCorrect === MAX_LETTERS;
+};
+
+const send = () => {
+  if (MAX_TRIES === totalTries || win) {
+    alert(`Game is over :( you ${win ? "WIN 🥳 " : "Loose  🥺"}`);
+    return;
+  }
+
+  if (MAX_LETTERS > totalLetters) {
     alert(`Hey! you need to type ${MAX_LETTERS - totalLetters} more letters`);
     return;
   }
 
-  
-  const letterBoard = document.getElementsByClassName(`letter-row-${totalTries}`);
-
-  secretword.forEach((letter, index) => {
-    switch (true) {
-      case letter === currentResponse[index]:
-        letterBoard[index].classList.add("correct");
-        break;
-      case currentResponse.includes(letter):
-        letterBoard[index].classList.add("wrong-position");
-        break;
-      default:
-        letterBoard[index].classList.add("incorrect");
-        break;
-    }
-  });
-
-  if(secretword.join() === currentResponse.join()) {
+  if (checkAnswer()) {
     win = true;
-    alert(`Congratulations, you WIN!!! 🥳`);
+    setTimeout(() => {
+      alert(`Congratulations, you WIN!!! 🥳`);
+    }, 100)    ;
   }
 
   totalTries++;
@@ -53,27 +55,31 @@ const send = () => {
   responses.push(currentResponse);
 
   currentResponse = [];
-}
+};
 
 const backspace = () => {
-  if(totalLetters === 0 || MAX_TRIES === totalTries || win) return;
-  const letterBoard = document.getElementsByClassName(`letter-row-${totalTries}`);
+  if (totalLetters === 0 || MAX_TRIES === totalTries || win) return;
+  const letterBoard = document.getElementsByClassName(
+    `letter-row-${totalTries}`
+  );
 
   const position = currentResponse.length - 1;
   letterBoard[position].innerText = "";
   totalLetters--;
   currentResponse.pop();
-}
+};
 
 const pressLetter = () => {
-  if(MAX_LETTERS === totalLetters || MAX_TRIES === totalTries || win) return;
-  const letterBoard = document.getElementsByClassName(`letter-row-${totalTries}`);
+  if (MAX_LETTERS === totalLetters || MAX_TRIES === totalTries || win) return;
+  const letterBoard = document.getElementsByClassName(
+    `letter-row-${totalTries}`
+  );
 
   const key = event.target;
   const letter = key.id;
 
   letterBoard[totalLetters].innerText = letter;
 
-  currentResponse.push(letter);  
+  currentResponse.push(letter);
   totalLetters++;
-}
+};
