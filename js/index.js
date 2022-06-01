bfa09adfb942ee601bbbeb94d1f776a41b49fc0d
Paola Alapizco <paolac.alapizco@gmail.com>
2022-06-01 11:33:11 -0600

    Dynamic board

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+const board = document.getElementById("board");
 const keyboard = document.getElementById("keyboard");
 
 const keyboardLetters = [
@@ -6,6 +7,7 @@ const keyboardLetters = [
   ["ENTER","Z","X","C","V","B","N","M","DELETE"],
 ];
 
+// Dynamic keyboard
 keyboardLetters.forEach(row => {
   const rowKey = document.createElement("div");
   rowKey.classList.add("keyboard-row");
@@ -32,3 +34,16 @@ keyboardLetters.forEach(row => {
   });
   keyboard.append(rowKey);
 });
+
+// Dynamic board
+for(let row = 0; row < 6; row++) {
+  const rowBoard = document.createElement("div");
+  rowBoard.classList.add("letter--container");
+
+  for(let col = 0; col < 5; col++) {
+    const colBoard = document.createElement("div");
+    colBoard.classList.add("letter");
+    rowBoard.append(colBoard);
+  }
+  board.append(rowBoard);
+}
